7181c15fcc2b59f6582a613e94b82bc1c6a07e95
Luke Baxter <lbb29@cam.ac.uk>
2021-02-04 13:15:36 +0000

    Added path

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const path = require('path');
 
 // Constants
 const PORT = 8080;
@@ -8,9 +9,7 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+app.get('/', express.static(path.join(__dirname, '../www')));
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);

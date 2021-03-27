f8a70ce403ace0c8822e4b86ca95b95b80477868
Luke Baxter <lbb29@cam.ac.uk>
2021-03-27 22:33:28 +0000

    Added desk background and color scheme

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import logMessage from './js/logger'
-import './css/style.css'
+import './css/style.scss'
 // Log message to console
 logMessage('Welcome to LukeBax.net!');
 
@@ -21,21 +21,11 @@ CSS.registerProperty( {
     }
 );
 
-const centerPanel = document.querySelector('.center-panel');
-let panelY = centerPanel.getBoundingClientRect().y;
-let panelX = centerPanel.getBoundingClientRect().x;
-let panelWidth = centerPanel.getBoundingClientRect().width;
-let panelHeight = centerPanel.getBoundingClientRect().height;
+// const centerPanel = document.querySelector('.headshot');
 
-window.addEventListener('resize', (e) => {
-  panelY = centerPanel.getBoundingClientRect().y;
-  panelX = centerPanel.getBoundingClientRect().x;
-  panelWidth = centerPanel.getBoundingClientRect().width;
-  panelHeight = centerPanel.getBoundingClientRect().height;
-});
-
-centerPanel.addEventListener('mousemove', (e) => {
-  const rotX = Math.sin(((e.pageX - panelX - (panelWidth/2))/(panelWidth/2))* Math.PI) * 20;
-  const rotY = Math.sin(((e.pageY - panelY - (panelHeight/2))/(panelHeight/2))* Math.PI) * 10;
-  centerPanel.style.transform = `rotateY(${rotX}deg) rotateX(${-rotY}deg)`;
-});
+// centerPanel.addEventListener('mousemove', (e) => {
+//   const panelRect = centerPanel.getBoundingClientRect();
+//   const rotX = Math.sin(((e.pageX - panelRect.x - (panelRect.width/2))/(panelRect.width/2))* Math.PI) * 20;
+//   const rotY = Math.sin(((e.pageY - panelRect.y - (panelRect.height/2))/(panelRect.height/2))* Math.PI) * 10;
+//   centerPanel.style.transform = `rotateY(${rotX}deg) rotateX(${-rotY}deg)`;
+// });

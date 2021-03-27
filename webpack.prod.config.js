bd19127bb9451f2786d7613d7b442a5009cf06f3
Luke Baxter <lbb29@cam.ac.uk>
2021-03-27 22:54:10 +0000

    Added scss for prod

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -58,6 +58,21 @@ module.exports = {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader']
       },
+      {
+       test: /\.(png|svg|jpg|gif)$/,
+       use: ['file-loader']
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      }
     ]
   },
   plugins: [
